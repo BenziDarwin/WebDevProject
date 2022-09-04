fccdfc1d5f908e69913dccc60b3e0c0160559485
Darwin Benzi <92586709+DarwinBenzi@users.noreply.github.com>
2022-09-04 19:31:00 +0300

    First commit

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,2 +1,41 @@
 
 users = []
+
+function imageHandler() {
+    image = document.getElementById("profilepic")
+    reader = new FileReader()
+    reader.onload = () => {
+        if(reader.readyState === 2) {
+            image.src = reader.result;
+        }
+    }
+    reader.readAsDataURL(event.target.files[0])
+}
+
+function checkPassword() {
+    users.push(event.target.value);
+    console.log(event.target.value)
+}
+
+function submitHandler() {
+    username = document.body.getElementById("username")
+    password = document.body.getElementById("password")
+    personalStatement = document.body.getElementById("personalStatement")
+    education = document.body.getElementById("education")
+    hobbies = document.body.getElementById ("hobbies")
+    user = {
+        name: username.value,
+        password: password.value,
+        personalStatement: personalStatement.value,
+        education: education.value,
+        hobbies: hobbies.value
+    }
+    users.push(user)
+    console.log(users)
+}
+list = []
+function hobbyHandler() {
+    hobby = document.getElementById("hobby")
+    list.push(hobby.value)
+    console.log(list)
+}
